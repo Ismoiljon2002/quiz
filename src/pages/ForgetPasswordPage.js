3b51cad03991527471a56d1a998b0b83f4062239
Ismoiljon Mirabdullaev <ismoiljo.20.02@gmail.com>
2023-04-11 09:25:26 +0500

    Login page redesigned

--- src/pages/ForgetPasswordPage.js
+++ src/pages/ForgetPasswordPage.js
@@ -2,6 +2,9 @@ import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
 import axios from 'axios';
 import './styles/Login.css';
+import { Form, Input, Button } from 'semantic-ui-react';
+// import logo from '../img/nuu_logo.png';
+import logo from '../img/logo-light.png';
 
 export default function ForgetPasswordPage() {
 
@@ -12,29 +15,34 @@ export default function ForgetPasswordPage() {
         console.log(email, "email");
 
         axios.post(`http://localhost:8080/api/forgot_password?email=${email}`)
-        .then(data => {
-            if ( data.status === 200 ) {
-                alert("reset success, now go to your email and smash the given link");
-            } else {
-                alert("Error! " + data.error)
-            }
-        })
+            .then(data => {
+                if (data.status === 200) {
+                    alert("reset success, now go to your email and smash the given link");
+                } else {
+                    alert("Error! " + data.error)
+                }
+            })
     }
 
 
     return (
-        <div className="forgot-password-page text-center m-5-auto">
-            <h2>Reset your password</h2>
-            <form action="/forgot_password" onSubmit={e => resetPassword(e)}>
-                <p>
-                    <label id="reset_pass_lbl">Email address</label><br />
-                    <input type="email" name="email" required onChange={e => setEmail(e.target.value)} />
-                </p>
-                <p>
-                    <button id="sub_btn" type="submit">Reset</button>
-                </p>
-                <p><Link to="/">Back to Login</Link>.</p>
-            </form>
+        <div className="login-page text-center m-5-auto">
+
+            <Form onSubmit={e => resetPassword(e)}>
+                <Form.Field required>
+
+                    <img src={logo} />
+                    <h2>Reset Your password</h2>
+
+                    <input
+                        type="email" className='input-email' placeholder='Email' onChange={e => setEmail(e.target.value)} required />
+
+                    <Button type='submit' size="big" color='green' className='reset-password-btn' fluid>Reset Password</Button>
+
+                    <Link to="/">Back to Login</Link>
+                </Form.Field>
+            </Form>
+
         </div>
     )
 }
--- src/pages/LoginPage.js
+++ src/pages/LoginPage.js
@@ -1,10 +1,13 @@
 import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
 import axios from 'axios';
+import { Form, Input, Button, Checkbox } from 'semantic-ui-react';
 
+// import logo from '../img/nuu_logo.png';
+import logo from '../img/logo-light.png';
 import './styles/Login.css';
 
-export default function SignInPage({setIsAuth}) {
+export default function SignInPage({ setIsAuth }) {
 
     const [username, setUsername] = useState("");
     const [password, setPassword] = useState("");
@@ -12,47 +15,54 @@ export default function SignInPage({setIsAuth}) {
     const checkLogin = e => {
         e.preventDefault();
         console.log(username, password)
-    
+
         axios.post("http://localhost:8080/api/signin/", {
-            username, 
+            username,
             password,
         })
-        .then(data => {
-            console.log(data.data, "came from user login...");
-            if ( data.data.status === 'OK') {
-                setIsAuth(true);
-                setTimeout(() => {
-                    alert("login success")
-                    window.localStorage.setItem("token", data.data.data)
-                    window.location.href = './userData';
-                }, 100);
+            .then(data => {
+                console.log(data.data, "came from user login...");
+                if (data.data.status === 'OK') {
+                    setIsAuth(true);
+                    setTimeout(() => {
+                        alert("login success")
+                        window.localStorage.setItem("token", data.data.data)
+                        window.location.href = './userData';
+                    }, 100);
 
-            } else {
-                alert("Error! " + data.data.error)
-            }
-        })
+                } else {
+                    alert("Error! " + data.data.error)
+                }
+            })
 
     }
 
     return (
         <div className="login-page text-center m-5-auto">
-            <h2>Login to your account</h2>
-            <form action="/" onSubmit={checkLogin}>
-                <p>
-                    <label>Username</label><br/>
-                    <input type="text" name="text" required onChange={e => setUsername(e.target.value)} />
-                </p>
-                <p>
-                    <label>Password</label>
-                    <Link to="/auth/forgot-password"><label className="right-label">Forgot password?</label></Link>
-                    <br/>
-                    <input type="password" name="password" required onChange={e => setPassword(e.target.value)} />
-                </p>
-                <p>
-                    <button id="sub_btn" type="submit">Login</button>
-                </p>
-                {/* <p>First time? <Link to="/auth/register">Create an account</Link>.</p> */}
-            </form>
+
+            <Form onSubmit={checkLogin}>
+                <Form.Field required>
+
+                    <img src={logo} />
+                    <h2>Login to Your account</h2>
+
+                    <input
+                        type="text" className='input-text' placeholder='Username' onChange={e => setUsername(e.target.value)} required/>
+                    <input className='input-password'
+                        type="password"
+                        placeholder="Password"
+                        onChange={e => setPassword(e.target.value)} required/>
+
+                    <React.Fragment>
+                        <Checkbox label="Remember Me" />
+
+                        <Link to="/auth/forgot-password">Forgot password?</Link>
+                    </React.Fragment>
+
+                    <Button type='submit' size="big" color='green' fluid>Login</Button>
+                </Form.Field>
+            </Form>
+
         </div>
     )
 }
