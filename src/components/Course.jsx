87c0cf524488e6d3d84f55d67f538b6dbfad1b94
Ismoiljon Mirabdullaev <ismoiljo.20.02@gmail.com>
2023-04-13 17:32:28 +0500

    Testing create exam paper

--- src/components/Course.jsx
+++ src/components/Course.jsx
@@ -5,17 +5,34 @@ import { Button, Card, Table } from "react-bootstrap";
 
 function Course({ course }) {
 
-    const [quizQuestions, setQuizQuestions] = useState([
-        {},
-    ])
+    
+    const [quizQuestions, setQuizQuestions] = useState(null)
+    let selectedQuestions = [];
+
+    const addToExamPaper = (e, id) => {
+        console.log(e.target)
+        if (e.target) selectedQuestions.push(id);
+        else selectedQuestions = selectedQuestions.filter(el => el !== id);
+    }
+
+    const sendExamPaper = () => {
+        axios.post('http://localhost:8080/api/questionPaperList/add/some4/1', {
+            examPapers: {
+                examType: "MIDTERM",
+                status: false,
+                variant: "A",
+            },
+            questionID: selectedQuestions
+        }).then(res => console.log("res ", res))
+    }
 
     useEffect(() => {
         axios.get("http://localhost:8080/api/course/questions/getQuestions/1")
             .then(res => {
-                console.log(res);
-                if (res.status === 200 ) setQuizQuestions(res);
+                console.log(res.data);
+                if (res.status === 200 ) setQuizQuestions(res.data);
                 else console.log(res.status)
-            }).catch(err => console.error("response error\n\n", err));
+            }).catch(err => console.log("response error\n", err));
     }, quizQuestions)
 
     return (
@@ -24,28 +41,31 @@ function Course({ course }) {
             <h3> Course Code: {course?.c_code} </h3>
             <h3> Course name: {course?.c_name} </h3>
 
-            <Table>
+            <Table stripped="true">
                 <thead>
                     <tr>
                         <th>question ID</th>
                         <th>Question</th>
                         <th>Type</th>
                         <th>Point</th>
+                        <th><input type="checkbox" name="" id="" /></th>
                     </tr>
                 </thead>
                 <tbody>
                     {quizQuestions?.map(q => 
-                    <tr>
+                    <tr key={q.question_id}>
                         <td>{ q.question_id }</td>
                         <td>{ q.question }</td>
                         <td> {q.qType} </td>
                         <td> {q.point} </td>
+                        <td><input type="checkbox" name="" id=""
+                            onClick={(e) => addToExamPaper(e, q.question_id)} /></td>
                     </tr>
                     )}
                 </tbody>
             </Table>
 
-            <Button>Get questions</Button>
+            <Button onClick={sendExamPaper}>Send Exam paper</Button>
         </Card>
     );
 }
